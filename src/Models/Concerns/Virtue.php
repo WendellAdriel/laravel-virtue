680a622311d1213b534ddc28a7a9080f8020e787
Wendell Adriel <wendelladriel.ti@gmail.com>
2024-05-06 12:26:47 +0100

    Fillable attribute

--- src/Models/Concerns/Virtue.php
+++ src/Models/Concerns/Virtue.php
@@ -4,12 +4,42 @@ declare(strict_types=1);
 
 namespace WendellAdriel\Virtue\Models\Concerns;
 
+use ReflectionClass;
+use WendellAdriel\Virtue\Models\Attributes\Fillable;
+
 trait Virtue
 {
     use HasRelations;
 
     public function initializeVirtue(): void
     {
+        $this->applyFillable();
+
         self::handleRelationsKeys($this);
+    }
+
+    private function applyFillable(): void
+    {
+        $fillableAttribute = $this->resolveFillableAttribute();
+        if (is_null($fillableAttribute)) {
+            return;
+        }
+
+        if ($fillableAttribute->fields !== []) {
+            $this->fillable($fillableAttribute->fields);
+        }
+    }
+
+    /**
+     * @return Fillable|null
+     */
+    private function resolveFillableAttribute(): ?Fillable
+    {
+        $reflectionClass = new ReflectionClass(static::class);
+        $primaryKeyAttribute = $reflectionClass->getAttributes(Fillable::class);
+
+        return $primaryKeyAttribute === []
+            ? null
+            : $primaryKeyAttribute[0]->newInstance();
     }
 }
